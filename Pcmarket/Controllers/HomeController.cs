eea19a9f27831cf1aa31262889e1be5d3395deca
Ilya <ilya1501andruhin@gmail.com>
2023-03-10 15:28:40 +0300

    Project

---
+++ Pcmarket/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Lymus_Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pcmarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pcmarket.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Pc pc = new Pc()
+            {
+                Name = "" ,
+                Model = ""
+            };
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
